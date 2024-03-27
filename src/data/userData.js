7567031e962937af2ad0bbf26c656267352a2658
milda_ruzgiene <milda.marcinkute@gmail.com>
2024-03-27 14:56:42 +0200

    cv filled from data, design colors and spacing

---
+++ src/data/userData.js
@@ -0,0 +1,53 @@
+import profileImg from '../assets/prof.jpg';
+
+export const USER_DATA = {
+  img: profileImg,
+  firstName: 'Milda',
+  lastName: 'Ruzgienė',
+  jobTitle: 'Junior FrontEnd Developer',
+  contacts: {
+    email: 'milda.marcinkute@gmail.com',
+    linkedIn: 'https://www.linkedin.com/in/milda-ruzgienė',
+    gitHub: 'https://github.com/MildaRuz',
+  },
+  skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Tailwind'],
+  languages: ['Lithuanian - native', 'English'],
+  interests: ['reading', 'walking', 'picking mushrooms', 'jewellery making', 'fishing'],
+  education: [
+    {
+      title: 'Junior FrontEnd Developer',
+      company: 'Baltic Institute of Technology',
+      year: 'Sep 2023 - Feb 2024',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    },
+    {
+      title: "Master's degree, Textile Engineering",
+      company: 'Kaunas University of Technology',
+      year: '2009 - 2011',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    },
+    {
+      title: "Bachelor's degree, Textile Engineering",
+      company: 'Kaunas University of Technology',
+      year: '2005 - 2009',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the',
+    },
+  ],
+  workingExperience: [
+    {
+      title: 'Technologist',
+      company: 'AB Audimas Supply',
+      year: 'Feb 2014 - Feb 2023',
+      description: '',
+    },
+    {
+      title: 'Technology Assistant',
+      company: 'AB Audimas',
+      year: 'Dec 2011 - Feb 2014',
+      description: '',
+    },
+  ],
+};
--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,24 +1,26 @@
 import '../App.css';
 
-import { interests, languages, skills } from '../helper';
+import { USER_DATA } from '../data/userData';
 
 export default function AboutMePage() {
   return (
     <div className="container-w mx-auto flex flex-row">
-      <div className="first-section py-11">
+      <div className="first-section py-16">
         <div>
-          <img src="https://picsum.photos/id/40/800/600" className="mx-auto" />
-          <h1 className="text-2xl mb-1 mt-3 uppercase text-center tracking-wide">Milda Ruzgienė</h1>
+          <img src={USER_DATA.img} className="mx-auto" />
+          <h1 className="text-2xl mb-1 mt-8 uppercase text-center tracking-wide">
+            {USER_DATA.firstName} {USER_DATA.lastName}
+          </h1>
           <div className="mb-8 text-center">
-            <em>Junior FrontEnd Developer</em>
+            <em>{USER_DATA.jobTitle}</em>
           </div>
           <div className="mx-28">
             <div>
-              <h2 className="text-xl uppercase underline underline-offset-4">contact</h2>
-              <div>email</div>
+              <h2 className="text-xl mt-16 my-4 uppercase underline underline-offset-4">contacts</h2>
+              <div>{USER_DATA.contacts.email}</div>
 
               <a
-                href="https://www.linkedin.com/in/milda-ruzgienė"
+                href={USER_DATA.contacts.linkedIn}
                 className="flex flex-row gap-2 items-center hover:text-[#0077b5] ease-out"
               >
                 LinkedIn{' '}
@@ -27,28 +29,45 @@ export default function AboutMePage() {
                   width="26"
                   height="26"
                   fill="currentColor"
-                  className="bi bi-linkedin "
+                  className="bi bi-linkedin my-4"
                   viewBox="0 0 16 16"
                 >
                   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                 </svg>
               </a>
+
+              <a
+                href={USER_DATA.contacts.gitHub}
+                className="flex flex-row gap-2 items-center hover:text-[#2dba4e] ease-out"
+              >
+                GitHub{' '}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="26"
+                  height="26"
+                  fill="currentColor"
+                  className="bi bi-github hover:text-[#2dba4e] my-4"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+              </a>
             </div>
-            <h2 className="text-xl uppercase underline underline-offset-4">skills</h2>
-            {skills.map((skill) => (
-              <div className="" key={skills.index}>
+            <h2 className="text-xl mt-16 uppercase underline underline-offset-4">skills</h2>
+            {USER_DATA.skills.map((skill) => (
+              <div className="my-4" key={USER_DATA.skills.index}>
                 <div>{skill}</div>
               </div>
             ))}
-            <h2 className="text-xl uppercase underline underline-offset-4">languages</h2>
-            {languages.map((lang) => (
-              <div className="" key={languages.index}>
+            <h2 className="text-xl mt-16 my-4 uppercase underline underline-offset-4">languages</h2>
+            {USER_DATA.languages.map((lang) => (
+              <div className="my-4" key={USER_DATA.languages.index}>
                 <div>{lang}</div>
               </div>
             ))}
-            <h2 className="text-xl uppercase">interests</h2>
-            {interests.map((hobbies) => (
-              <div className="" key={interests.index}>
+            <h2 className="text-xl mt-16 uppercase underline">interests</h2>
+            {USER_DATA.interests.map((hobbies) => (
+              <div className="my-4" key={USER_DATA.interests.index}>
                 <div>{hobbies}</div>
               </div>
             ))}
@@ -56,26 +75,30 @@ export default function AboutMePage() {
         </div>
       </div>
       <div className="second-section py-16 px-28">
-        <h2 className="text-xl uppercase underline underline-offset-4">working experience</h2>
-        <div className="text-lg uppercase">Job title</div>
-        <div>
-          <em>Company | 2011-2021</em>
-        </div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-          make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
-        </p>
-        <h2 className="text-xl uppercase underline underline-offset-4">education</h2>
-        <div className="text-lg uppercase">degree name</div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-          make a type specimen book.
-        </p>
+        <h2 className="text-xl mt-16 uppercase underline underline-offset-4">education</h2>
+
+        {USER_DATA.education.map((ed) => (
+          <div key={USER_DATA.education.index}>
+            <div className="text-lg uppercase mt-10">
+              <em>
+                {ed.company} | <span className="normal-case">{ed.year}</span>{' '}
+              </em>
+            </div>
+            <div className="mt-10 leading-7">{ed.description}</div>
+          </div>
+        ))}
+
+        <h2 className="text-xl mt-16 uppercase underline underline-offset-4">working experience</h2>
+        {USER_DATA.workingExperience.map((workEx) => (
+          <div key={USER_DATA.workingExperience.index}>
+            <div className="text-lg uppercase mt-10">
+              <em>
+                {workEx.company} | <span className="normal-case">{workEx.year}</span>
+              </em>
+            </div>
+            <div className="mt-10 leading-7">{workEx.description}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
