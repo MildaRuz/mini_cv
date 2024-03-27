c4e9ea93ebc5d0d8fb5d169eea43776520613898
milda_ruzgiene <milda.marcinkute@gmail.com>
2024-03-27 10:58:27 +0200

    start page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import AboutMePage from './pages/AboutMePage';
+
+function App() {
+  return (
+    <>
+      <AboutMePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/helper.js
@@ -0,0 +1,3 @@
+export const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Tailwind'];
+export const languages = ['Lithuanian - native', 'English'];
+export const interests = ['reading', 'picking mushrooms', 'fishing'];
